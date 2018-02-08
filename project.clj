8fb35fb0e5d2d0925faf32b40090a1ab9e8622bc
Andrew Nguyen <andrew.nguyen@usfca.edu>
2018-02-08 09:43:15 -0800

    Fork of 0.9.0
    - Update clojure to 1.9.0
    - Changed project to `andrew-nguyen/conch`
    
    Passes 16 tests/47 assertions via `lein test`

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject me.raynes/conch "0.9.0"
+(defproject andrew-nguyen/conch "0.9.1"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "https://github.com/Raynes/conch"
+  :url "https://github.com/andrew-nguyen/conch"
   :description "A better shell-out library for Clojure."
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :aliases {"testall" ["with-profile" "dev,default:dev,1.5,default:dev,1.4,default" "test"]}
   :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
